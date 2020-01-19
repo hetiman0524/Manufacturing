730c5f21462997308f6f5e966a97efa5139feba8
hetiman0524 <heti0524.jdi@gmail.com>
2020-01-20 02:57:34 +0900

    マイグレーションファイル削除

--- db/migrate/20200113054151_rename_paper_roll_mumber_column_to_manufacturing_statuses.rb
+++
@@ -1,5 +0,0 @@
-class RenamePaperRollMumberColumnToManufacturingStatuses < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :manufacturing_statuses, :paper_roll_mumber, :paper_roll_number
-  end
-end
